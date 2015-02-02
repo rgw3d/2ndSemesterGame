ac00e4daa2c62039ef4aa3935d66246409518707
rgw3d <rgw3d@aol.com>
2015-02-02 10:44:48 -0500

    Everything

---
+++ Assets/Grid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	Vector3 gridSize = new Vector3(1,1,1);
+	Vector3 movementDirection = new Vector3(0,0,1);
+    
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = transform.position+movementDirection;
+		newPos = new Vector3(Mathf.Round(newPos.x/gridSize.x)*gridSize.x,
+		                 Mathf.Round(newPos.y/gridSize.y)*gridSize.y,
+		                 Mathf.Round(newPos.z/gridSize.z)*gridSize.z);
+		transform.TransformPoint (newPos);
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Sprite front;
+    public Sprite back;
+    public Sprite left;
+    public Sprite right;
+    private Sprite currentSprite;
+	SpriteRenderer spriteRenderer;
+
+	public float movementDistance = .05f;
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = front;
+        currentSprite = front;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+            rigidbody2D.AddForce(Vector2.up * movementDistance);
+            if (currentSprite != back){
+                spriteRenderer.sprite = back;
+                currentSprite = back;
+            }
+		}
+		if (Input.GetKey (KeyCode.S)) {
+            rigidbody2D.AddForce(Vector2.up * -1 * movementDistance);
+			//transform.Translate(0,-movementDistance,0);
+            if (currentSprite != front){
+                spriteRenderer.sprite = front;
+                currentSprite = front;
+            }
+		}
+		if (Input.GetKey (KeyCode.A)) {
+            rigidbody2D.AddForce(Vector2.right * -1 * movementDistance);
+			//transform.Translate(-movementDistance,0,0);
+            if (currentSprite != left){
+                spriteRenderer.sprite = left;
+                currentSprite = left;
+            }
+
+		}
+		if (Input.GetKey (KeyCode.D)) {
+            rigidbody2D.AddForce(Vector2.right * movementDistance);
+			//transform.Translate(movementDistance,0,0);
+            if(currentSprite != right){
+                spriteRenderer.sprite = right;
+                currentSprite = right;
+            }
+		}
+	}
+}
