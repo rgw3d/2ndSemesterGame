d8ac45663e29513b5166098b3e330db64b442b45
Steven Leonis <Spwizzard2@gmail.com>
2015-02-06 10:32:25 -0500

    Better scaling of character

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -31,14 +31,16 @@ public class NewBehaviourScript : MonoBehaviour {
 	void Update () {
 		if (Input.GetKey (KeyCode.W)) {
             //transform.Translate(0, movementDistance, 0);
-            rigidbody2D.AddForce(Vector2.up * upMovement);
+			if(transform.position.y<yMax)
+           		rigidbody2D.AddForce(Vector2.up * upMovement);
             if (currentSprite != back){
                 //spriteRenderer.sprite = back;
                 //currentSprite = back;
             }
 		}
 		if (Input.GetKey (KeyCode.S)) {
-            rigidbody2D.AddForce(Vector2.up * -1 * upMovement);
+			if(transform.position.y>yMin)
+            	rigidbody2D.AddForce(Vector2.up * -1 * upMovement);
 			//transform.Translate(0,-movementDistance,0);
             if (currentSprite != front){
                 //spriteRenderer.sprite = front;
@@ -64,7 +66,7 @@ public class NewBehaviourScript : MonoBehaviour {
 		}
 
         float distToBot = Mathf.Abs(yMin - transform.position.y);
-        Vector3 newScale = new Vector3(-.5f * distToBot + .885f,-.125f * distToBot + .885f,1);
+        Vector3 newScale = new Vector3(-.3f * distToBot + 2f,-.3f * distToBot + 2f,1);
         transform.localScale = newScale;
         
         ///if ymin = -1 is scale of 1, and 1 is scale of .75  (-1,1) and (1,.75)
